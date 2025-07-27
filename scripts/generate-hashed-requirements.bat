3e66e1a9e2f1d1312e84718272f8d6d37c39918c
Aaron Zielstorff <aaron.zi@web.de>
2025-07-27 10:11:21 +0000

    Improves docker build workflow

--- scripts/generate-hashed-requirements.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Script to generate requirements.txt with hashes for better supply chain security
-
-pip-compile --generate-hashes --output-file requirements-hashed.txt requirements.txt
--- scripts/generate-hashed-requirements.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script to generate requirements.txt with hashes for better supply chain security
-
-pip-compile --generate-hashes --output-file requirements-hashed.txt requirements.txt
